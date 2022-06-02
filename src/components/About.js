05898f7b31c25427fda80ccb0394fe1e03bc1fe9
Iain <60200813+iainwh@users.noreply.github.com>
2022-06-02 09:25:24 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className='about'>
             <h2>We are anonymous! We are legion! Expect us!</h2>
             <p>
-            The planet is a friggin shitshow. We need to change it through code.
+            The planet is a friggin shitshow. We need to change it through code!!!
             </p>
         </section>
     );
