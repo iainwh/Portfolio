dae4f2f5f877bf585a11b7aa2cc1870ca13080d1
Iain <60200813+iainwh@users.noreply.github.com>
2022-06-10 08:02:54 +0100

    Edit Friday 10th June 8am

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import profile from '../assets/profile.png';
 const About = () => {
     return (
         <section className='about'>
-            <h2>We are anonymous! We are legion! Expect us!</h2>
+            <h2>We are anonymous!!!! We are legion! Expect us!</h2>
             <p>
             The planet is a friggin shitshow!!! We need to change it through code!!!
             </p>
